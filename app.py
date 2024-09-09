1b0df748b97628bc6c55029b87105bfe642b0705
Lucas David <89425766+LucasDavid80@users.noreply.github.com>
2024-09-09 14:41:05 -0300

    Aliviando erro de usuario

--- app.py
+++ app.py
@@ -76,7 +76,10 @@ def receive_text():
     text_mensage = data.get('text_mensage')
 
     if not user_id or user_id not in usuarios: 
-        return jsonify({"error": "Usuário não encontrado"}), 400
+        return jsonify({
+            "error": "Usuário não encontrado",
+            "response_text": response_text
+                        }), 400
 
     estado_conversa = usuarios[user_id]
  
